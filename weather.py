e3c320d070a5bb7b5e9ae5124450468d7d607573
sadeg1377 <sadeghhadipour1@gmail.com>
2024-04-17 13:03:13 +0330

    first commit

---
+++ weather.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city='london'):
+    request_url = f"http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric"
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+
+if __name__ == '__main__':
+    print("\n*** Get Current Weather Conditions ***\n")
+
+    city = input("\nPlease enter a city name\n")
+
+    # Check for empty strings of string with only spaces
+    if not bool(city.strip()):
+        city = 'london'
+
+    request_url = f"http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric"
+
+    weather_data = requests.get(request_url).json()
+
+    pprint(weather_data)
